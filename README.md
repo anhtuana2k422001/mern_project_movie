a4adada8f7b9846b77d6a26689b7879fbe89f9f1
anhtuana2k422001 <anhtuana2k422001@gmail.com>
2023-02-24 23:43:51 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# The Movie Database (TMDB), một dịch vụ cung cấp thông tin về phim ảnh.
+# Time Start: 24 - 02 - 2023 
+# Author: Hồ Anh Tuấn - Võ Anh Quân - Phan Thị Kim Thoa
---
+++ server/src/tmdb/tmdb.endpoints.js
@@ -0,0 +1,37 @@
+import tmdbConfig from "./tmdb.config.js";
+
+// The Movie Database (TMDB) - một dịch vụ cung cấp thông tin về phim ảnh.
+const tmdbEndpoints = {
+    // Lấy ra URL danh sách media 
+    // props là mediaType, mediaCategory, và page ==> Component ReactJS 
+    MediaList: ({ mediaType, mediaCategory, page }) => tmdbConfig.getUrl(
+        `${mediaType}/${mediaCategory}`, page
+    ),
+    mediaDetail: ({ mediaType, mediaId }) => tmdbConfig.getUrl(
+        `${mediaType}/${mediaCategory}/${mediaId}`
+    ),
+    mediaGenres: ({ mediaType }) => tmdbConfig.getUrl(
+        `genre/${mediaType}/list`
+    ),
+    mediaCredits: ({ mediaType, mediaId }) => tmdbConfig.getUrl(
+        `${mediaType}/${mediaId}/credits`
+    ),
+    mediaVideos: ({ mediaType, mediaId }) => tmdbConfig.getUrl(
+        `${mediaType}/${mediaId}/videos`
+    ),
+    mediaRecommend: ({ mediaType, mediaId }) => tmdbConfig.getUrl(
+        `${mediaType}/${mediaId}/recommendations`
+    ),
+    mediaImages: ({ mediaType, mediaId }) => tmdbConfig.getUrl(
+        `${mediaType}/${mediaId}/images`
+    ),
+    mediaSearch: ({ mediaType, query, page }) => tmdbConfig.getUrl(
+        `search/${mediaType}`, {query, page}
+    ),
+    personDetail: ({ personID }) => tmdbConfig.getUrl(
+        `person/${personID}`
+    ),
+    personMedias: ({ personID }) => tmdbConfig.getUrl(
+        `person/${personID}/combined_credits`
+    ),
+};
