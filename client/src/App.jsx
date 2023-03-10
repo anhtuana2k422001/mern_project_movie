e43ad8ede2333f6be150ef22be13331f61d91615
LAPTOP-MTGPP3N7\Admin <phanthikimthoa.ltp@gmail.com>
2023-03-10 14:43:44 +0700

    mern_movie

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,28 @@
+import {ThemeProvider }from "@mui/material/styles";
+import {useSelector} from "react-redux"
+import themeConfigs from "./configs/theme.configs";
+import{ToasContainer} from "react-toastify";
+import CssBaseLine from "@mui/material/CssBaseLine";
 const App = () => {
+  const{themeMode} = useSelector((state ) => state.themeMode)
   return (
-    <div>
-      App
-    </div>
+    <ThemeProvider theme ={themeConfigs.custom({mode: themeMode})}>
+      {/* config toastify*/}
+      <ToasContainer
+      position="bottom-left"
+      autoClose={5000}
+      hideProgressBar={false}
+      newesOnTop={false}
+      closeOnClick
+      pauseOnFocusLoss
+      pauseOnHover
+      theme={themeMode}
+
+      />
+      {/* mui reset css */}
+      <CssBaseLine/>
+        
+    </ThemeProvider>
   );
 }
 
